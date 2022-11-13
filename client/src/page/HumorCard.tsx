f08645c92d32a9a17779c9991b869d4da741faf6
Jihoon Kim <kzc123@snu.ac.kr>
2022-11-13 22:50:34 +0900

    Update humor card

--- client/src/page/HumorCard.tsx
+++ client/src/page/HumorCard.tsx
@@ -1,15 +1,23 @@
-import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Rating } from '@mui/material';
+import { FaQuoteLeft } from 'react-icons/fa';
+
 
 const HumorCard = (props: any) => {
   const { humor } = props;
 
-  return <div className='mb-2 d-grid'>
-    <Button
-      variant='light'
-    >
-      {humor.content}
-    </Button>
-  </div>
+  return <Card className='mb-2 d-grid'>
+    <Card.Body>
+      <FaQuoteLeft className='mb-3' />
+      <div>
+        {humor.content}
+      </div>
+      <hr />
+      <div className='d-flex justify-content-end' >
+        <Rating defaultValue={Number(humor.score)} precision={0.5} readOnly size='small' />
+      </div>
+    </Card.Body>
+  </Card>
 }
 
 export default HumorCard;

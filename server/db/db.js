c8c0a7e9bc65be09b71e9ab9f7f0673f28959f5f
Jihoon Kim <kzc123@snu.ac.kr>
2022-11-13 22:25:10 +0900

    Add getUser by id

--- server/db/db.js
+++ server/db/db.js
@@ -3,13 +3,17 @@ import db_info from './db_info.js';
 
 const pool = new pg.Pool(db_info);
 
+export const getUser = async (user_id) => {
+  const result = await pool.query(`SELECT * FROM public.user WHERE id = ${user_id}`);
+  return result.rows[0];
+};
+
 export const getUsers = async () => {
   const result = await pool.query('SELECT * FROM public.user');
   return result.rows;
 };
 
 export const getHumorsByUser = async (user_id) => {
-  console.log(`SELECT * FROM user_humor WHERE user_id = ${user_id}`);
   const result = await pool.query(`SELECT * FROM user_humor WHERE user_id = ${user_id}`);
   return result.rows;
 };
--- server/router/api.js
+++ server/router/api.js
@@ -1,4 +1,14 @@
 export default (app, db) => {
+  app.get('/api/user/:user_id', async (req, res) => {
+    try {
+      const result = await db.getUser(req.params.user_id);
+      res.status(200).json(result);
+    } catch (error) {
+      console.log('getUser', error);
+      res.sendStatus(400);
+    }
+  });
+
   app.get('/api/users', async (req, res) => {
     try {
       const result = await db.getUsers();
